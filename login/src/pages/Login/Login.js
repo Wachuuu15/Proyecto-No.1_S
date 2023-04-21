d5105b4243416a5209ff51a9c0696f657572c6dc
wachuuu <88355602+Wachuuu15@users.noreply.github.com>
2023-04-21 13:14:36 -0600

    Login

--- login/src/pages/Login/Login.js
+++ login/src/pages/Login/Login.js
@@ -1,18 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 import Title from "./components/Title/Title";
 import Label from "./components/Label/Label";
 import Input from "./components/input/input";
+import '../Login/Login.css'
+import "./components/input/input.css";
+
 
 const Login = ()=>{
-    return(
-        <div className="login-container">
-            <Title text='Soy un title'/>
-            <Label text='Email'/>
-            <Input  />
-            <Label text='Password'/>
-            <Input  />
 
+    const[ Email, setEmail] = useState('');
+    const[ Password, setPassword] = useState('');
+
+    function handleChange(name,value){
+        if(name === 'Email'){
+            setEmail(value)
+        }else{
+            setPassword(value)
+        }
+    };
+
+    function handleSubmit(){
+        let account = {Email, Password}
+        if(account){
+            console.log('account:', account)
+        }
+    };
+
+    return(
+        <div className="contenedor-general">
+              <div className='login-container'>
+                <Title text='Soy un title'/>
+                <Label text='Email'/>
+                <Input  
+                attribute={{
+                    id:'Email',
+                    name:'Email',
+                    type:'Email',
+                    placeholder:'Email'
+                }}
+                handleChange={handleChange}
+
+                />
+                <Label text='Password'/>
+                <Input  
+                attribute={{
+                    id:'Password',
+                    name:'Password',
+                    type:'password',
+                    placeholder:'Password'
+                }}
+                handleChange={handleChange}
+                />
+               <div className="button-container">
+               <button onClick={(e) => handleSubmit} className="submit-button">
+                    Log in
+                </button>
+               </div>
+            </div>
+            <div className='imagen-formulario'>
+
+            </div> 
         </div>
+      
     )
 };
 
--- login/src/pages/Login/components/input/input.jsx
+++ login/src/pages/Login/components/input/input.jsx
@@ -2,10 +2,17 @@ import React from "react";
 import '../input/input.css';
 
 
-const Input = () =>{
-    return(
-        <div>
-            <input />
+const Input = ({attribute, handleChange, param}) =>{
+    return (
+        <div className='input-container'>
+            <input
+            id={attribute.id}
+            name={attribute.name}
+            placeholder={attribute.placeholder}
+            type={attribute.type}
+            onChange={ (e) => handleChange(e.targetname, e.target.value)}
+            className='regular-style'
+            />
         </div>
     )
 };
