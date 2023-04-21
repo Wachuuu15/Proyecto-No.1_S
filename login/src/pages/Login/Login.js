4c69472d7baefae9e36f28debc432974fce01663
wachuuu <88355602+Wachuuu15@users.noreply.github.com>
2023-04-20 23:50:40 -0600

    react

---
+++ login/src/pages/Login/Login.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Title from "./components/Title/Title";
+import Label from "./components/Label/Label";
+
+const Login = ()=>{
+    return(
+        <div>
+            <Title text='Soy un title'/>
+            <Label text='Email'/>
+            <Label text='Password'/>
+
+        </div>
+    )
+};
+
+export default Login;
